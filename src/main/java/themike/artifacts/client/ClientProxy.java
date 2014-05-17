93b8c6b38c568047b8949ff47f4c6bef56312f19
michael <zeusintraining@gmail.com>
2014-05-17 18:18:30 -0400

    Working on Stone Golems, still.

--- src/main/java/themike/artifacts/client/ClientProxy.java
+++ src/main/java/themike/artifacts/client/ClientProxy.java
@@ -1,13 +1,17 @@
 package themike.artifacts.client;
 
+import cpw.mods.fml.client.registry.RenderingRegistry;
 import net.minecraftforge.client.MinecraftForgeClient;
+import themike.artifacts.client.entity.RenderStoneGolem;
 import themike.artifacts.common.CommonProxy;
+import themike.artifacts.entities.EntityStoneGolem;
 
 public class ClientProxy extends CommonProxy {
 	
 	@Override
 	public void init() {
 		super.init();
+		RenderingRegistry.registerEntityRenderingHandler(EntityStoneGolem.class, new RenderStoneGolem());
 	}
 
 }
--- src/main/java/themike/artifacts/client/entity/RenderStoneGolem.java
+++ src/main/java/themike/artifacts/client/entity/RenderStoneGolem.java
@@ -15,12 +15,14 @@ public class RenderStoneGolem extends RenderIronGolem {
 		super();
 	}
 	
-    protected ResourceLocation getEntityTexture(EntityStoneGolem entity) {
+	@Override
+    protected ResourceLocation getEntityTexture(EntityIronGolem entity) {
         return new ResourceLocation("artifacts", "textures/entity/stone_golem.png");
     }
 	
-    public void doRender(EntityStoneGolem entity, double blarg1, double blarg2, double blarg3, float blarg4, float blarg5) {
-		BossStatus.setBossStatus(entity, false);
+    @Override
+    public void doRender(EntityIronGolem entity, double blarg1, double blarg2, double blarg3, float blarg4, float blarg5) {
+		BossStatus.setBossStatus((EntityStoneGolem) entity, false);
 		super.doRender(entity, blarg1, blarg2, blarg3, blarg4, blarg5);
 	}
 
