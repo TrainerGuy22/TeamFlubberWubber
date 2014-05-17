9e2014f9a17dd1c55341c554ec7561649fda5d46
michael <zeusintraining@gmail.com>
2014-05-17 19:18:11 -0400

    More work.

---
+++ src/main/java/themike/artifacts/blocks/tile/TileDisplayGlass.java
@@ -0,0 +1,27 @@
+package themike.artifacts.blocks.tile;
+
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntity;
+
+public class TileDisplayGlass extends TileEntity {
+	
+	public int artifactMetadata;
+	
+	public TileDisplayGlass() {
+	}
+	
+	public void updateEntity() {
+		
+	}
+	
+	@Override
+	public void writeToNBT(NBTTagCompound tag) {
+		
+	}
+	
+	@Override
+	public void readFromNBT(NBTTagCompound tag) {
+		
+	}
+
+}
