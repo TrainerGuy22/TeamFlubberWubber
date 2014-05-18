5e177ba2c7a08dbda17903a84faf142f6d6f6051
michael <zeusintraining@gmail.com>
2014-05-18 12:00:20 -0400

    I think I'm doing this right.

--- src/main/java/themike/artifacts/world/WorldGenArtifactDungeon.java
+++ src/main/java/themike/artifacts/world/WorldGenArtifactDungeon.java
@@ -2,7 +2,9 @@ package themike.artifacts.world;
 
 import java.util.Random;
 
+import themike.artifacts.common.CommonProxy;
 import themike.artifacts.util.Coord2;
+import net.minecraft.init.Blocks;
 import net.minecraft.util.ChunkCoordinates;
 import net.minecraft.world.World;
 import net.minecraft.world.chunk.IChunkProvider;
@@ -17,6 +19,8 @@ public class WorldGenArtifactDungeon implements IWorldGenerator {
 	}
 	
 	public boolean canSpawn(World world, int x, int y, int z) {
+		if(CommonProxy.dungeon_world_data.artifacts[artifactMetadata] == 1)
+			return false;
 		if(world.getBlock(x + 6, y - 2, z - 3).isAir(world, x + 6, y - 2, z - 3) ||
 		   world.getBlock(x + 6, y - 2, z + 3).isAir(world, x + 6, y - 2, z - 3) ||
 		   world.getBlock(x - 3, y - 2, z - 3).isAir(world, x + 6, y - 2, z - 3) ||
@@ -32,19 +36,41 @@ public class WorldGenArtifactDungeon implements IWorldGenerator {
 
 	@Override
 	public void generate(Random random, int chunkX, int chunkZ, World world, IChunkProvider generator, IChunkProvider provider) {
+		if(world.provider.dimensionId != 0) {
+			return;
+		}
+		
 		ChunkCoordinates spawnCoords = world.getSpawnPoint();
 		
-		int x = chunkX * 16;
-		int z = chunkZ * 16;
+		int x = chunkX * 16 + random.nextInt(16);
+		int z = chunkZ * 16 + random.nextInt(16);
 		int y = 10 + random.nextInt(7);
 
 		Coord2 spawn = new Coord2(spawnCoords.posX, spawnCoords.posZ);
-		Coord2 chunk = new Coord2(x, z);
+		Coord2 chunk = new Coord2(chunkX * 16, chunkZ * 16);
 		
 		// if(spawn.distance(chunk) <= 2000) {
 		if(random.nextInt(60) == 4 && canSpawn(world, x, y, z)) {
-				
+			CommonProxy.dungeon_world_data.artifacts[artifactMetadata] = 1;
+			
+			for(int countX = x - 3; countX != x + 6; countX++) {
+				for(int countZ = z - 3; countZ != z + 4; countZ++) {
+					for(int countY = y - 1; countY != y + 4; countY++) {
+						world.setBlock(countX, countY, countZ, Blocks.air);
+					}
+					world.setBlock(countX, y - 2, countZ, Blocks.stonebrick);
+				}
+			}
+			
+			world.setBlock(x, y, z, CommonProxy.display_glass);
+			world.setBlock(x, y - 1, z, Blocks.quartz_block, 2, 3);
+			
+			world.setBlock(x - 1, y - 2, z - 1, Blocks.glowstone);
+			world.setBlock(x - 1, y - 2, z + 1, Blocks.glowstone);
+			world.setBlock(x + 1, y - 2, z - 1, Blocks.glowstone);
+			world.setBlock(x + 1, y - 2, z + 1, Blocks.glowstone);
 		}
+		
 		// }
 		
 		
